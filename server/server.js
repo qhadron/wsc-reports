db5dce3e9a68bcb4ab34cb1e64f42483e859c4d9
qhadron <jack.lee2980@gmail.com>
2017-08-31 12:07:12 -0400

    Cleaned up configurations

--- server/server.js
+++ server/server.js
@@ -9,24 +9,41 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
 const APPROOT = process.cwd();
 const configpath = path.resolve(APPROOT, 'config', 'serverconfig');
+const config = (() => {
+    try {
+        return require(configpath);
+    } catch (e) {
+        console.log(`Invalid server configuration, see ${configpath}.js.sample for more details`);
+        return {
+            port: process.env.PORT || 3000,
+            host: undefined
+        }
+    }
+})();
 
 const debug = require('debug')('server');
+const port = config.port;
+const host = config.host;
+let server = app;
+
+const mounted_callback = () => {
+    debug(`Server listening at ${server.address().family} ${server.address().address} ${server.address().port}`)
+};
 
 try {
-    const {ssl_key_path, ssl_cert_path} = require(configpath);
+    const {ssl_key_path, ssl_cert_path} = config;
 
-    https.createServer({
+    server = https.createServer({
         key: fs.readFileSync(ssl_key_path),
         cert: fs.readFileSync(ssl_cert_path)
-    }, app).listen(port, () => {
-        debug(`Express ssl server listening on port ${port}`);
-    });
+    }, app);
 } catch (err) {
-    console.warn(`Error reading ssl configuration. Edit ${configpath}.js to enable ssl...`);
-    app.listen(port, () => {
-        debug(`Express server listening on port ${port}`);
-    });
+    console.log(`Error reading ssl configuration. Edit ${configpath}.js to enable ssl...`);
 }
+
+if (host) 
+    server = server.listen(port, host, mounted_callback);
+else 
+    server = server.listen(port, mounted_callback);
